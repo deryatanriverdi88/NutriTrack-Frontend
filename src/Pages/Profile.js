62580b756589343ebe4718ac37d9b3150b5ccae0
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-09-30 23:44:40 -0400

    Adds logOut props to Profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-function  Profile(){
+function  Profile({logOut}){
 
 const current_user = useSelector(state => {
     // debugger 
