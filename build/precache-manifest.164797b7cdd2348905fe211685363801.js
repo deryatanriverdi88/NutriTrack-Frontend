39bbb4ca39b2a5c5797e1890e91fa7326f8b1470
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-09-30 21:39:11 -0400

    modifies build

---
+++ build/precache-manifest.164797b7cdd2348905fe211685363801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0450237e693e42399063fa5eb19767c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f69ba5831cb37aeb236d",
+    "url": "/static/css/main.1cd3558c.chunk.css"
+  },
+  {
+    "revision": "1c4bb8d0cb8e1835052c",
+    "url": "/static/js/2.c9fe74a6.chunk.js"
+  },
+  {
+    "revision": "f69ba5831cb37aeb236d",
+    "url": "/static/js/main.46638c2e.chunk.js"
+  },
+  {
+    "revision": "1117caf84b005bd9fde3",
+    "url": "/static/js/runtime-main.c794b379.js"
+  },
+  {
+    "revision": "7c1fa49952ea57c5818900c23ca0be78",
+    "url": "/static/media/nutritions-4.7c1fa499.jpeg"
+  }
+]);
